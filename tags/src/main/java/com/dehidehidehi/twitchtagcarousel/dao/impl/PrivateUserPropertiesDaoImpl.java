3d73e09e47eb0c3561bf77034737247b4f05dffc
Kevin <digitalexmachina@gmail.com>
2023-04-07 21:42:52 +0200

    implemented user access token dao

--- tags/src/main/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PrivateUserPropertiesDaoImpl.java
+++ tags/src/main/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PrivateUserPropertiesDaoImpl.java
@@ -2,34 +2,60 @@ package com.dehidehidehi.twitchtagcarousel.dao.impl;
 
 import com.dehidehidehi.twitchtagcarousel.dao.PrivateUserPropertiesDao;
 import com.dehidehidehi.twitchtagcarousel.error.TwitchMissingAuthTokenException;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Typed;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Typed(PrivateUserPropertiesDao.class)
 @ApplicationScoped
-class PrivateUserPropertiesDaoImpl implements PrivateUserPropertiesDao {
+class PrivateUserPropertiesDaoImpl implements PrivateUserPropertiesDao, AutoCloseable {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PrivateUserPropertiesDaoImpl.class);
-	
-	private String userAccessToken;
+	static final String PRIVATE_USER_PROPERTIES_FILE = "/private-user.properties";
+	static final String PROPERTY_TWITCH_ACCESS_TOKEN = "twitch.access-token";
+	private Properties properties;
+	private InputStream propertiesInputStream;
+
+	@PostConstruct
+	void init() {
+		properties = new Properties();
+		try {
+			propertiesInputStream = getClass().getResourceAsStream(PRIVATE_USER_PROPERTIES_FILE);
+			properties.load(this.propertiesInputStream);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	// TODO simple caching mecanism
 	@Override
 	public String getUserAccessToken() throws TwitchMissingAuthTokenException {
-		if (userAccessToken == null) {
+		final String accessToken = properties.getProperty(PROPERTY_TWITCH_ACCESS_TOKEN);
+		if (StringUtils.isEmpty(accessToken)) {
 			throw new TwitchMissingAuthTokenException("Missing user access token! Did you query it before calling this method?");
 		}
-		return userAccessToken;
+		return accessToken;
 	}
 
 	@Override
 	public void setUserAccessToken(final String userAccessToken) {
 		final String hiddenToken = StringUtils.abbreviateMiddle(userAccessToken, "******", 12);
+		properties.setProperty(PROPERTY_TWITCH_ACCESS_TOKEN, userAccessToken);
 		LOGGER.info("Successfully received access token :)");
 		LOGGER.debug("Received access token {}", hiddenToken);
-		// todo save it in user config files?
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (propertiesInputStream != null) {
+			propertiesInputStream.close();
+		}
 	}
 }
---
+++ tags/src/test/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PrivateUserPropertiesDaoImplTest.java
@@ -0,0 +1,62 @@
+package com.dehidehidehi.twitchtagcarousel.dao.impl;
+import com.dehidehidehi.twitchtagcarousel.dao.PrivateUserPropertiesDao;
+import com.dehidehidehi.twitchtagcarousel.error.TwitchMissingAuthTokenException;
+import com.dehidehidehi.twitchtagcarousel.util.CDIExtension;
+import jakarta.inject.Inject;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+import static com.dehidehidehi.twitchtagcarousel.dao.impl.PrivateUserPropertiesDaoImpl.PRIVATE_USER_PROPERTIES_FILE;
+import static com.dehidehidehi.twitchtagcarousel.dao.impl.PrivateUserPropertiesDaoImpl.PROPERTY_TWITCH_ACCESS_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(CDIExtension.class)
+class PrivateUserPropertiesDaoImplTest {
+    
+    private final String defaultTwitchAccessTokenValue = "this_is_my_access_token!";
+    
+    @Inject
+    private PrivateUserPropertiesDao privateUserPropertiesDao;
+
+    @BeforeEach
+    void setUp() throws URISyntaxException, IOException {
+        setUpPropertiesFileWithDefaultValues();
+    }
+
+    private void setUpPropertiesFileWithDefaultValues() throws URISyntaxException, IOException {
+        final URI fileUri = PrivateUserPropertiesDaoImpl.class.getResource(PRIVATE_USER_PROPERTIES_FILE).toURI();
+        final File propertiesFile = new File(fileUri);
+        propertiesFile.createNewFile();
+        try (FileOutputStream fileOutputStream = new FileOutputStream(propertiesFile)) {
+            final String defaultContent = "%s=%s".formatted(PROPERTY_TWITCH_ACCESS_TOKEN, defaultTwitchAccessTokenValue);
+            fileOutputStream.write(defaultContent.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    @Order(1)
+    @Test
+    void getUserAccessTokenShouldReturnAccessTokenFromPropertiesFile() throws TwitchMissingAuthTokenException {
+        assertThatCode(() -> privateUserPropertiesDao.getUserAccessToken()).doesNotThrowAnyException();
+        assertThat(privateUserPropertiesDao.getUserAccessToken())
+                .isNotNull()
+                .isEqualTo(defaultTwitchAccessTokenValue);
+    }
+
+    @Test
+    void setUserAccessTokenShouldChangeAccessTokenInPropertiesFile() throws TwitchMissingAuthTokenException {
+        final String randomToken = RandomStringUtils.random(20);
+        assertThatCode(() -> privateUserPropertiesDao.setUserAccessToken(randomToken)).doesNotThrowAnyException();
+        assertThat(privateUserPropertiesDao.getUserAccessToken())
+                .isEqualTo(randomToken);
+    }
+}
