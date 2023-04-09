bf68aa5aa3d9875cb354be08c3766a79e28dbfcc
Ke30 <email@email.com>
2023-04-09 17:08:07 +0200

    start up token validation now works :)

--- tags/src/main/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PrivateUserPropertiesDaoImpl.java
+++ tags/src/main/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PrivateUserPropertiesDaoImpl.java
@@ -15,46 +15,51 @@ import java.util.Properties;
 
 @Typed(PrivateUserPropertiesDao.class)
 @ApplicationScoped
-class PrivateUserPropertiesDaoImpl implements PrivateUserPropertiesDao, AutoCloseable {
+class PrivateUserPropertiesDaoImpl extends PropertiesDaoUtil implements PrivateUserPropertiesDao, AutoCloseable {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PrivateUserPropertiesDaoImpl.class);
-	static final String PRIVATE_USER_PROPERTIES_FILE = "/private-user.properties";
-	static final String PROPERTY_TWITCH_ACCESS_TOKEN = "twitch.access-token";
-	private Properties properties;
-	private InputStream propertiesInputStream;
+    static final String PRIVATE_USER_PROPERTIES_FILE = "/private-user.properties";
+    static final String PROPERTY_TWITCH_ACCESS_TOKEN = "twitch.access-token";
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrivateUserPropertiesDaoImpl.class);
+    private Properties properties;
+    private InputStream propertiesInputStream;
 
-	@PostConstruct
-	void init() {
-		properties = new Properties();
-		try {
-			propertiesInputStream = getClass().getResourceAsStream(PRIVATE_USER_PROPERTIES_FILE);
-			properties.load(this.propertiesInputStream);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @PostConstruct
+    void init() {
+        properties = new Properties();
+        propertiesInputStream = getClass().getResourceAsStream(PRIVATE_USER_PROPERTIES_FILE);
+        try {
+            properties.load(propertiesInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	public String getUserAccessToken() throws MissingAuthTokenException {
-		final String accessToken = properties.getProperty(PROPERTY_TWITCH_ACCESS_TOKEN);
-		if (StringUtils.isEmpty(accessToken)) {
-			throw new MissingAuthTokenException("Missing user access token! Did you query it before calling this method?");
-		}
-		return accessToken;
-	}
+    @Override
+    public String getUserAccessToken() throws MissingAuthTokenException {
+        final String accessToken = properties.getProperty(PROPERTY_TWITCH_ACCESS_TOKEN);
+        if (StringUtils.isEmpty(accessToken)) {
+            throw new MissingAuthTokenException("Missing user access token! Did you query it before calling this method?");
+        }
+        return accessToken;
+    }
 
-	@Override
-	public void setUserAccessToken(final String userAccessToken) {
-		final String hiddenToken = StringUtils.abbreviateMiddle(userAccessToken, "******", 12);
-		properties.setProperty(PROPERTY_TWITCH_ACCESS_TOKEN, userAccessToken);
-		LOGGER.info("Successfully received access token :)");
-		LOGGER.debug("Received access token {}", hiddenToken);
-	}
+    @Override
+    public void setUserAccessToken(final String userAccessToken) {
+        final String hiddenToken = StringUtils.abbreviateMiddle(userAccessToken, "******", 12);
+        properties.setProperty(PROPERTY_TWITCH_ACCESS_TOKEN, userAccessToken);
+        updatePropertiesFile();
+        LOGGER.info("Successfully received access token :)");
+        LOGGER.debug("Received access token {}", hiddenToken);
+    }
 
-	@Override
-	public void close() throws Exception {
-		if (propertiesInputStream != null) {
-			propertiesInputStream.close();
-		}
-	}
+    private void updatePropertiesFile() {
+        updatePropertiesFile(properties, PROPERTY_TWITCH_ACCESS_TOKEN);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (propertiesInputStream != null) {
+            propertiesInputStream.close();
+        }
+    }
 }
---
+++ tags/src/main/java/com/dehidehidehi/twitchtagcarousel/dao/impl/PropertiesDaoUtil.java
@@ -0,0 +1,28 @@
+package com.dehidehidehi.twitchtagcarousel.dao.impl;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
+abstract class PropertiesDaoUtil {
+
+	protected void updatePropertiesFile(final Properties properties, final String filePath) {
+		final URL resource = getClass().getResource(filePath);
+		final URI uri;
+		try {
+			uri = resource.toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+		final File file = new File(uri);
+		try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+			properties.store(fileOutputStream, null);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
